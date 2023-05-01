5355df3878971e1c3eca92defb09bc4f73627d46
Alder998 <davide.ontano@yahoo.com>
2023-05-01 17:15:28 +0200

    V3 Data Update 2

--- Forza 4 Bot/generaDatabase.py
+++ Forza 4 Bot/generaDatabase.py
@@ -2,9 +2,15 @@
 import Forza4Methods as f4
 import pandas as pd
 
-f4.updateDataBase(50000)
+#f4.updateDataBase(50000)
 
-df = pd.read_excel(r"C:\Users\39328\OneDrive\Desktop\Davide\Velleità\Forza 4 Algoritmo\Dataset\finalDataSet.xlsx")
+#df = pd.read_excel(r"C:\Users\39328\OneDrive\Desktop\Davide\Velleità\Forza 4 Algoritmo\Dataset\finalDataSet.xlsx")
 
-#f4.createModel(df, 50000, version='V2')
+df = pd.read_excel(r"C:\Users\39328\OneDrive\Desktop\Davide\Velleità\Forza 4 Algoritmo\Dataset\finalDataSetV3.xlsx")
+
+df['Win'] = df['Win1']
+del[df['Win1']]
+del[df['WIn2']]
+
+f4.createModel(df, 2100, version='V3')
 
